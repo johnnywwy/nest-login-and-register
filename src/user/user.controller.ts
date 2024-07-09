438d55db854208bcbc35e087709de579caab1195
wuwenyu <johnnywwy@gmail.com>
2024-07-09 15:50:01 +0800

    feat: 实现登录注册

---
+++ src/user/user.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
+
+  @Post('login')
+  async login(@Body() user: LoginDto) {
+    const foundUser = await this.userService.login(user);
+
+    if (foundUser) {
+      return '登录成功';
+    } else {
+      return '登录失败';
+    }
+  }
+
+  @Post('register')
+  async register(@Body() user: RegisterDto) {
+    console.log('register', user);
+    return await this.userService.register(user);
+  }
+}
